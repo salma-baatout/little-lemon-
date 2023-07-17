cbecff4cefa10e195a4495fe6967d909b2df2645
sarahalsaeedi <7628021+sarahalsaeedi@users.noreply.github.com>
2023-07-17 21:30:55 +0200

    app screens

--- App.js
+++ App.js
@@ -1,19 +1,49 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, SafeAreaView } from "react-native";
 import OnBoarding from "./screens/OnBoarding";
+import Profile from "./screens/Profile.js";
+import Splash from "./screens/Splash";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Home from "./screens/Home";
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <OnBoarding />
+    <SafeAreaView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Splash">
+          <Stack.Screen
+            name="Profile"
+            component={Profile}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="Onboarding"
+            component={OnBoarding}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="Splash"
+            component={Splash}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
